716300ac0664e93d78e4a28f6d5a0fadea8961af
llx <535921901@qq.com>
2016-04-15 15:14:11 +0800

    上传

--- LaunchDemo/ViewController.swift
+++ LaunchDemo/ViewController.swift
@@ -20,6 +20,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        launchAnimation()
+    }
 
+    private func launchAnimation()
+    {
+        
+        let vc = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateViewControllerWithIdentifier("Launch")
+        let launchview = vc.view
+        let delegate = UIApplication.sharedApplication().delegate
+        let mainWindow = delegate?.window
+        mainWindow!!.addSubview(launchview)
+        
+        
+        UIView.animateWithDuration(1, delay: 0.5, options: UIViewAnimationOptions.BeginFromCurrentState, animations: {
+            launchview.alpha = 0.0
+            launchview.layer.transform = CATransform3DScale(CATransform3DIdentity, 1.5, 1.5, 1.0)
+        }) { (finished) in
+            launchview.removeFromSuperview()
+        }
+        
+        
+        
+    }
+    
 }
 
